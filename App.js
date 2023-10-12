91383cec6934ab685c87779c0d3fcc76dc92739a
tickle35 <79730838+tickle35@users.noreply.github.com>
2023-10-12 21:38:01 +0000

    Food DetailScreen Added

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from './screens/HomeScreen';
+import Details from './screens/Details';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
        <Stack.Screen name="HomePage" component={HomeScreen}/>
+       <Stack.Screen name="Details" component={Details}/>
       </Stack.Navigator>
     </NavigationContainer>
     
