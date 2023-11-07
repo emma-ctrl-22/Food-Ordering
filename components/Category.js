561f80f2cc3cf1cfafe1af3ecc1edbb79ce0eb53
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2023-11-07 15:18:18 +0000

    Update Category.js

--- components/Category.js
+++ components/Category.js
@@ -10,8 +10,8 @@ export default function Category() {
         gap:3
     }}
     showsHorizontalScrollIndicator={false} style={{padding:1,margin:3,width:"98%"}}>
-      <CateoryCard onPress title="testing" imgUrl="https://deepcaves.world/images/events-min.jpg"/>
-      <CateoryCard title="testing" imgUrl="https://deepcaves.world/images/events-min.jpg"/>
+      <CateoryCard onPress title="Snacks" imgUrl="https://deepcaves.world/images/events-min.jpg"/>
+      <CateoryCard title="Groceries " imgUrl="https://deepcaves.world/images/events-min.jpg"/>
       <CateoryCard title="testing" imgUrl="https://deepcaves.world/images/events-min.jpg"/>
       <CateoryCard title="testing" imgUrl="https://deepcaves.world/images/events-min.jpg"/>
       <CateoryCard title="testing" imgUrl="https://deepcaves.world/images/events-min.jpg"/>
