5d7368325fbb06f7915db4f89411af51581d8dee
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2023-11-09 21:44:52 +0000

    Friday

---
+++ screens/Admin.js
@@ -0,0 +1,12 @@
+import { View, Text ,StyleSheet, Image, TextInput, ScrollView,SafeAreaView} from 'react-native'
+import React, { useLayoutEffect } from 'react'
+
+const AdminScreen = () =>{
+    return(
+        <SafeAreaView>
+
+        </SafeAreaView>
+    )
+}
+
+export default AdminScreen;
